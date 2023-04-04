042eae5918773c67a801966ede3b2157120981e2
Bobbi Towers <porkostomus@mail.com>
2023-04-03 19:51:30 -0700

    Update graph.clj

--- src/exercism/concepts/graph.clj
+++ src/exercism/concepts/graph.clj
@@ -1,34 +1,58 @@
-(ns exercism.concepts.graph
-  (:require [ring.adapter.jetty :as jetty]
+(ns web-dev.core
+  (:require [ring.adapter.jetty :refer [run-jetty]]
             [ring.middleware.resource :refer [wrap-resource]]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.util.response :refer [response content-type]]
+            [clojure.pprint     :as pprint]
+            [web-dev.graph :as graph]
+            [clojure.data.json :as json]
             [com.phronemophobic.clj-graphviz :refer [render-graph]]
-            [hiccup.page :refer [html5]]
-            [clojure.data.json :as json]))
+            [hiccup.page :refer [html5]]))
 
-(def exercises (:concept (:exercises (json/read-str (slurp "https://raw.githubusercontent.com/exercism/clojure/main/config.json") :key-fn keyword))))
+(defn exercises [track]
+  (let [url (str "https://raw.githubusercontent.com/exercism/"
+                 track "/main/config.json")]
+    (-> url
+        slurp
+        (json/read-str :key-fn keyword)
+        :exercises
+        :concept)))
 
 (defn concept
   "Returns a sequence of the exercises that teach a concept
   or have it as a prerequisite, according to key provided."
-  [c k]
-  (map :name (filter #(contains? (set (k %)) c) exercises)))
+  [track c k]
+  (map :name (filter #(contains? (set (k %)) c) (exercises track))))
 
-(defn nodes [c]
-  (mapv vector (cycle (concept c :concepts))
-       (concept c :prerequisites)))
+(defn nodes [track c]
+  (mapv vector (cycle (concept track c :concepts))
+       (concept track c :prerequisites)))
 
-(render-graph {:edges (vec (mapcat nodes (set (mapcat :concepts exercises))))}
-               {:filename "resources/public/graph.png"})
+(defn render [track edges]
+  (render-graph {:edges edges}
+                {:filename (str "resources/public/" track ".png")}))
 
-(def page
-  (html5 {:lang "en"}
-    [:body [:div [:h1 "Clojure syllabus"]
-                 [:img {:src "graph.png"}]]]))
+(defn page [name]
+  (str "<html><body>"
+       (if name
+         (let [img (render name (vec (mapcat #(nodes name %) (set (mapcat :concepts (exercises name))))))]
+           (str "<div> <h1> " name " syllabus</h1>
+            <img src=" name ".png" ">"))
+           (str "<form>"
+                "Track name (e.g. javascript): <input name='name' type='text'>"
+                "<input type='submit'>"
+                "</form>"))
+         "</body></html>"))
 
-(defn handler [request]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body page})
+(page "clojure")
 
-(defn -main []
-  (jetty/run-jetty (wrap-resource handler "public") {:port 80}))
+(defn handler [{{name "name"} :params}]
+  (-> (response (page name))
+      (content-type "text/html")))
+
+(def app
+  (-> handler
+      (wrap-resource "public")
+      wrap-params))
+
+(run-jetty app {:port 80})
